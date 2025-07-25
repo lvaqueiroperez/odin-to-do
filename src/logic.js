045c6f67ec3af36535af77723dca08001af88063
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-07-25 18:07:26 +0200

    Create global tasks array

--- src/logic.js
+++ src/logic.js
@@ -60,6 +60,7 @@ const homePageModule = (function () {
     }
 
     let projectsArray = [new Project("project1", "sample project"), new Project("project2", "sample project 2"), new Project("project3", "sample project 3")];
+    let globalTasksArray = [new Task("task1", "task test 1", "01/01/2026", false), new Task("task3", "task test 3", "01/01/2026", false), new Task("task3", "taks test 3", "01/01/2026", false)];
 
     console.log("**************WELCOME**************");
 
