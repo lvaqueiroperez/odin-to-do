41e726961492f1ae16490190a8cd87f5616aab03
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-07-25 16:21:29 +0200

    Create homepage module with search project method

--- src/logic.js
+++ src/logic.js
@@ -1,56 +1,68 @@
 // TODO: Poner argumentos opcionales
+const homePageModule = (function () {
 
-class Project {
+    class Project {
 
-    tasks = [new Task("task1", "sample task", "01/01/2026", true)];
+        tasks = [new Task("task1", "sample task", "01/01/2026", true)];
 
-    constructor(name, description) {
+        constructor(name, description) {
 
-        this.name = name;
-        this.description = description;
+            this.name = name;
+            this.description = description;
 
-    }
+        }
 
-    deleteTask(idToDelete) {
-        this.tasks.splice(this.tasks.findIndex((task) => task.id === idToDelete), 1);
-        console.log("Current tasks: \n" + this.tasks);
-    }
+        deleteTask(idToDelete) {
+            this.tasks.splice(this.tasks.findIndex((task) => task.id === idToDelete), 1);
+            console.log("Current tasks: \n" + this.tasks);
+        }
 
-    showTasks() {
-        console.table(this.tasks);
-    }
+        showTasks() {
+            console.log("PROJECT: " + this.name);
+            console.table(this.tasks);
+        }
 
-    addTask(newTask) {
-        this.tasks.push(newTask);
-    }
-
-}
-
-class Task {
-
-    #id = crypto.randomUUID();
-
-    constructor(title, description, dueDate, priority) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.done = false;
+        addTask(newTask) {
+            this.tasks.push(newTask);
+        }
 
     }
 
+    class Task {
 
-    getId() {
-        return this.id;
+        #id = crypto.randomUUID();
+
+        constructor(title, description, dueDate, priority) {
+            this.title = title;
+            this.description = description;
+            this.dueDate = dueDate;
+            this.priority = priority;
+            this.done = false;
+
+        }
+
+
+        getId() {
+            return this.id;
+        }
+
     }
 
-}
+    let projectsArray = [new Project("project1", "sample project")];
+
+    console.log("**************WELCOME**************");
+
+    console.table(projectsArray);
+
+    const searchProject = (string) => {
+        return projectsArray.find((project) => {
+            project.name.toLowerCase() === string.toLowerCase();
+        });
+    }
+
+})();
 
 
 
 
-
-
-export default function greet1() {
-    console.log("this is logic, roger!");
-}
+export { homePageModule };
