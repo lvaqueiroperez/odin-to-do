9944d0db28bb8f4ab7a82d2ead0b9543449fe8c1
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-08-18 10:57:22 +0200

    Create and implement new ES6 module for classes

---
+++ src/TaskProjectClasses.js
@@ -0,0 +1,80 @@
+const Project = class {
+
+    #tasks = [];
+
+    constructor(name, description) {
+
+        this.name = name;
+        this.description = description;
+        this._id = crypto.randomUUID();
+
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    deleteTask(idToDelete) {
+        this.#tasks.splice(this.#tasks.findIndex((task) => task.id === idToDelete), 1);
+        console.log("Current tasks: \n" + this.#tasks);
+    }
+
+    getTasks() {
+        // console.log("PROJECT NAME: " + this.name);
+        // console.table(this.#tasks);
+        return this.#tasks;
+    }
+
+    addTask() {
+
+        const taskName = prompt("Please, enter the new task's name");
+        const taskDescription = prompt("Please, enter the new task's description");
+        const taskDueDate = prompt("Please, enter the new task's due date");
+        const taskPriority = prompt("Does this new task have priority? yes/no") === "yes" ? true : false;
+
+        const newTask = new Task(taskName, taskDescription, taskDueDate, taskPriority);
+        newTask.setProjectId(this.id);
+
+        this.#tasks.push(newTask);
+
+        console.log("NEW TASK ADDED");
+        console.table(this.#tasks);
+
+    }
+
+    pushTask(newTask) {
+        newTask.setProjectId(this.id);
+        this.#tasks.push(newTask);
+    }
+
+}
+
+const Task = class {
+
+    #taskId = crypto.randomUUID();
+
+    constructor(title, description, dueDate, priority, projectId = "global") {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.done = false;
+        this.projectId = projectId;
+    }
+
+
+    getTaskId() {
+        return this.#taskId;
+    }
+
+    getProjectId() {
+        return this.projectId;
+    }
+
+    setProjectId(projectId) {
+        this.projectId = projectId;
+    }
+
+}
+
+export { Project, Task }; 
--- src/logic.js
+++ src/logic.js
@@ -1,86 +1,8 @@
 // Me mola mucho este estilo de programación modular, creo funcionalidades como módulos
 // que luego serán fáciles de añadir junto al DOM!
+import { Project, Task } from "./TaskProjectClasses";
+
 const homePageModule = (function () {
-
-    class Project {
-
-
-        #tasks = [];
-
-        constructor(name, description) {
-
-            this.name = name;
-            this.description = description;
-            this._id = crypto.randomUUID();
-
-        }
-
-        get id() {
-            return this._id;
-        }
-
-        deleteTask(idToDelete) {
-            this.#tasks.splice(this.#tasks.findIndex((task) => task.id === idToDelete), 1);
-            console.log("Current tasks: \n" + this.#tasks);
-        }
-
-        getTasks() {
-            // console.log("PROJECT NAME: " + this.name);
-            // console.table(this.#tasks);
-            return this.#tasks;
-        }
-
-        addTask() {
-
-            const taskName = prompt("Please, enter the new task's name");
-            const taskDescription = prompt("Please, enter the new task's description");
-            const taskDueDate = prompt("Please, enter the new task's due date");
-            const taskPriority = prompt("Does this new task have priority? yes/no") === "yes" ? true : false;
-
-            const newTask = new Task(taskName, taskDescription, taskDueDate, taskPriority);
-            newTask.setProjectId(this.id);
-
-            this.#tasks.push(newTask);
-
-            console.log("NEW TASK ADDED");
-            console.table(this.#tasks);
-
-        }
-
-        pushTask(newTask) {
-            newTask.setProjectId(this.id);
-            this.#tasks.push(newTask);
-        }
-
-    }
-
-    class Task {
-
-        #taskId = crypto.randomUUID();
-
-        constructor(title, description, dueDate, priority, projectId = "global") {
-            this.title = title;
-            this.description = description;
-            this.dueDate = dueDate;
-            this.priority = priority;
-            this.done = false;
-            this.projectId = projectId;
-        }
-
-
-        getTaskId() {
-            return this.#taskId;
-        }
-
-        getProjectId() {
-            return this.projectId;
-        }
-
-        setProjectId(projectId) {
-            this.projectId = projectId;
-        }
-
-    }
 
     let projectsArray = [new Project("project1", "sample project"), new Project("project2", "sample project 2"), new Project("project3", "sample project 3")];
     let globalTasksArray = [new Task("task1", "task test 1", "01/01/2026", false), new Task("task2", "task test 3", "01/01/2026", false), new Task("task3", "taks test 3", "01/01/2026", false)];
