f701b376dca0e61454a7a910967eef0ef8729372
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-08-20 11:24:56 +0200

    Create and implement search btn event listener + A lot of code in this commit, my fault

--- src/dom.js
+++ src/dom.js
@@ -46,7 +46,64 @@ const homePageDOM = (function () {
 
     }
 
-    return { loadHomepageProjectList, loadProjectSearchDatalist };
+    const loadHomepageEventListeners = function () {
+
+        const searchButton = document.querySelector(".searchProject>button");
+        const homepageProjectListContainer = document.querySelector(".homepageProjectListContainer");
+        const globalProjectButtonContainer = document.querySelector(".globalProjectButtonContainer");
+
+        searchButton.addEventListener("click", (e) => {
+
+            const projectName = document.querySelector("input[type='search']").value;
+
+            const foundProjects = homePageModule.searchProject(projectName);
+
+            if (foundProjects.length > 0) {
+
+                loadProjectSearchResults(foundProjects);
+
+            } else {
+                // todo: mensaje "no se ha encontrado ningún proyecto con ese nombre"
+                alert("We couldn't find any project with that name.");
+                loadHomepageProjectList();
+            }
+
+        });
+
+    }
+
+    function loadProjectSearchResults(foundProjects) {
+
+        const homepageProjectListContainer = document.querySelector(".homepageProjectListContainer");
+
+        removeElementChildren(homepageProjectListContainer);
+
+        foundProjects.forEach((project) => {
+
+            const projectButton = document.createElement("button");
+            projectButton.textContent = project.name;
+            projectButton.setAttribute("data-projectId", project.id);
+
+            homepageProjectListContainer.appendChild(projectButton);
+
+        });
+
+        const eraseResultsButton = document.createElement("button");
+        eraseResultsButton.textContent = "erase results";
+
+        homepageProjectListContainer.appendChild(eraseResultsButton);
+
+        // todo: open and close event listener when clicked or another search it's done, or
+        // when going to global proyect
+        eraseResultsButton.addEventListener("click", (e) => {
+
+            loadHomepageProjectList();
+
+        }, { once: true });
+
+    }
+
+    return { loadHomepageProjectList, loadProjectSearchDatalist, loadHomepageEventListeners };
 
 })();
 
