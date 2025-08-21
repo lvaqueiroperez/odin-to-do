1b3ce9ff70191734595563af233f15dad60b551a
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-08-21 11:07:16 +0200

    Close dialog after project was created

--- src/dom.js
+++ src/dom.js
@@ -148,6 +148,7 @@ const homePageDOM = (function () {
                     loadHomepageProjectList();
                     loadProjectSearchDatalist();
                     loadGlobalProject();
+                    homepageCreateProjectDialog.close();
             }
 
         });
